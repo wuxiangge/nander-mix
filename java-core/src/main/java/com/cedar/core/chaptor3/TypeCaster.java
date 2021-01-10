58287a65223d05b35545be385150c9b08a17bf45
zhangnan <1343887538@qq.com>
2021-01-10 23:13:36 +0800

    java 虚拟机

--- java-core/src/main/java/com/cedar/core/chaptor3/TypeCaster.java
+++ java-core/src/main/java/com/cedar/core/chaptor3/TypeCaster.java
@@ -1,5 +1,8 @@
 package com.cedar.core.chaptor3;
 
+import java.io.Console;
+import java.util.Locale;
+
 public class TypeCaster {
 
 
@@ -7,5 +10,29 @@ public class TypeCaster {
         int n = 123456789;
         float f = n;
         System.out.println(f);
+
+
+
+
+        String str = "greeting";
+        String substring = str.substring(0, 3);
+        System.out.println(substring);
+        int i = str.codePointCount(0, str.length());
+        System.out.println(i);
+
+        System.out.println(str.lastIndexOf("t"));
+
+        System.out.println(str.substring(0,str.length()-1));
+
+        System.out.println(str.toUpperCase());
+
+
+        Console console = System.console();
+//        String s = console.readLine("user name:");
+//        char[] chars = console.readPassword("password:");
+
+        String property = System.getProperty("user.dir");
+        System.out.println(property);
+
     }
 }
