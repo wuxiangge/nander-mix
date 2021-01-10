a918a63abc5392fca3f56fa155ce9a68000d34b2
zhangnan <1343887538@qq.com>
2021-01-10 21:22:33 +0800

    java 多线程

---
+++ java-core/src/main/java/com/cedar/core/chaptor3/TypeCaster.java
@@ -0,0 +1,11 @@
+package com.cedar.core.chaptor3;
+
+public class TypeCaster {
+
+
+    public static void main(String[] args) {
+        int n = 123456789;
+        float f = n;
+        System.out.println(f);
+    }
+}
