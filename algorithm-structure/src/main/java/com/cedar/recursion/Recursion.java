a5665e2286ec4597f2bd2d76d20d32794cf234e6
zhangnan <1343887538@qq.com>
2021-02-24 22:14:07 +0800

    添加--添加尚硅谷netty

--- algorithm-structure/src/main/java/com/cedar/recursion/Recursion.java
+++ algorithm-structure/src/main/java/com/cedar/recursion/Recursion.java
@@ -88,7 +88,68 @@ public class Recursion {
         Integer[] list = {1, 2, 3};
         recursion.perm(list, 0, 2);
 
-        recursion.hanoi(3,1,1,1);
+        recursion.hanoi(3, 1, 1, 1);
+
+    }
+
+
+    /**
+     * 两个矩阵乘机所需要的的计算量
+     *
+     * @param A
+     * @param B
+     * @param C
+     * @param ARow
+     * @param AColumn
+     * @param BRow
+     * @param BColumn
+     */
+    public void matrixMultiply(int[][] A, int[][] B, int[][] C, int ARow, int AColumn, int BRow, int BColumn) {
+
+        if (AColumn != BRow) {
+            throw new IllegalArgumentException("矩阵不可乘");
+        }
+
+        for (int i = 0; i < ARow; i++) {
+            for (int j = 0; j < BColumn; j++) {
+                int sum = A[i][0] * B[0][j];
+                for (int k = 1; k < AColumn; k++) {
+                    sum += A[i][k] * B[k][j];
+                }
+                C[i][j] = sum;
+            }
+        }
+    }
+
+    public void matrixChain(int[] p, int n, int[][] m, int[][] s) {
+        for (int i = 1; i <= n; i++) {
+            m[i][i] = 0;
+        }
+        for (int r = 2; r <= n; r++) {
+            for (int i = 1; i <= n - r + 1; i++) {
+                int j = i + r - 1;
+                m[i][j] = m[i + 1][j] + p[i - 1] * p[i] * p[j];
+
+                s[i][j] = i;
+
+                for (int k = i + 1; k < j; k++) {
+                    int t = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+                    if (t < m[i][j]) {
+                        m[i][j] = t;
+                        s[i][j] = k;
+                    }
+                }
+            }
+        }
+    }
+
+    public void traceBack(int i,int j ,int[][] s) {
+        if (i==j) {
+            return;
+        }
+
+        traceBack(i,s[i][j],s);
+        traceBack(s[i][j]+1,j,s);
 
     }
 
