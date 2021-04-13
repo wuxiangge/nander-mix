c1c38fcaaec2866578954cb823a0d3a8132cdb63
zhangnan <1343887538@qq.com>
2021-04-13 19:31:14 +0800

    添加--添加多线程

--- concurrency-art/src/main/java/com/cedar/concurrency/art/SynchronousQueueDemo.java
+++ concurrency-art/src/main/java/com/cedar/concurrency/art/SynchronousQueueDemo.java
@@ -1,5 +1,7 @@
 package com.cedar.concurrency.art;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.TimeUnit;
 
@@ -7,6 +9,8 @@ public class SynchronousQueueDemo {
 
 
     public static void main(String[] args) throws Exception {
+
+        new BufferedReader(new InputStreamReader(System.in));
 
         SynchronousQueue<String> queue = new SynchronousQueue<String>();
 
