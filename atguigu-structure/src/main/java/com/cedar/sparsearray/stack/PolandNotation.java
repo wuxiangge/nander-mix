bac2ebd5cc326d4cd56bbdf4d668f2bce452ec0f
zhangnan <1343887538@qq.com>
2021-04-19 00:39:54 +0800

    约瑟夫环 逆波兰表达式

--- atguigu-structure/src/main/java/com/cedar/sparsearray/stack/PolandNotation.java
+++ atguigu-structure/src/main/java/com/cedar/sparsearray/stack/PolandNotation.java
@@ -1,12 +1,41 @@
 package com.cedar.sparsearray.stack;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * @author zhangnan
  * @date 2021-04-18 22:00
  */
 public class PolandNotation {
+
+    private static final String REGEXP_OPERATION = "\\+|-|\\*|\\/|\\(|\\)";
+
+    private static final String REGEXP_ADD_MINUS = "\\+|-";
+
+    private static final String REGEXP_TIMES_DIVISION = "\\/|\\*";
+
+    /**
+     * 加減 + -
+     */
+    private static final int LEVEL_01 = 1;
+    /**
+     * 乘除 * /
+     */
+    private static final int LEVEL_02 = 2;
+
+    /**
+     * 括号
+     */
+    private static final int LEVEL_HIGH = Integer.MAX_VALUE;
+
+
+    private static final String LEFT = "(";
+    private static final String RIGHT = ")";
+    private static final String ADD = "+";
+    private static final String MINUS = "-";
+    private static final String TIMES = "*";
+    private static final String DIVISION = "/";
 
 
     // 中缀转后缀
@@ -21,7 +50,6 @@ public class PolandNotation {
     }
 
 
-
     // 外部调用
     public int calculate(String expression) {
 
@@ -32,6 +60,38 @@ public class PolandNotation {
     // 内部调用
     private int calculate(List<String> suffixExpressionList) {
 
+    }
+
+
+    // 去除字符串中的所有空格
+    public static String replaceAllWhite(String expression) {
+        return expression.replaceAll("\\s+", "");
+    }
+
+    // 判断数字正则表达式
+    public static boolean isNumber(String expression) {
+        Pattern pattern = Pattern.compile("^[-\\+]?\\d+(\\.\\d+)?$");
+        return pattern.matcher(expression).matches();
+    }
+
+    public static boolean isOperation(String expression) {
+        return expression.matches(REGEXP_OPERATION);
+    }
+
+
+    /**
+     * 匹配运算等级
+     *
+     * @param operation 操作符
+     * @return
+     */
+    public static int calcLevel(String operation) {
+        if (operation.matches(REGEXP_ADD_MINUS)) {
+            return LEVEL_01;
+        } else if (operation.matches(REGEXP_TIMES_DIVISION)) {
+            return LEVEL_02;
+        }
+        return LEVEL_HIGH;
     }
 
 
