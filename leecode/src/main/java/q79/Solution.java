b66ec7af07d7abb202ad1b1a15ec7c2bc1060f5d
zhangnx <zhangnx@servyou.com.cn>
2021-05-07 15:36:36 +0800

    动态规划问题解决

--- leecode/src/main/java/q79/Solution.java
+++ leecode/src/main/java/q79/Solution.java
@@ -1,17 +1,68 @@
+//给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。如果 word 存在于网格中，返回 true ；否则，返回 false 。
+//
+// 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "AB
+//CCED"
+//输出：true
+//
+//
+// 示例 2：
+//
+//
+//输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SE
+//E"
+//输出：true
+//
+//
+// 示例 3：
+//
+//
+//输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "AB
+//CB"
+//输出：false
+//
+//
+//
+//
+// 提示：
+//
+//
+// m == board.length
+// n = board[i].length
+// 1 <= m, n <= 6
+// 1 <= word.length <= 15
+// board 和 word 仅由大小写英文字母组成
+//
+//
+//
+//
+// 进阶：你可以使用搜索剪枝的技术来优化解决方案，使其在 board 更大的情况下可以更快解决问题？
+// Related Topics 数组 回溯算法
+// 👍 883 👎 0
+
+
 package q79;
 
 /**
  * @author zhangnan
- * @date 2021-04-29 22:36
+ * @date 2021/4/28 11:34
  */
 public class Solution {
 
-    public boolean exist(char[][] board, String word) {
+    private static final int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
+    public boolean exist(char[][] board, String word) {
         int h = board.length;
         int w = board[0].length;
 
         boolean[][] visited = new boolean[h][w];
+
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
                 boolean flag = check(board, visited, i, j, word, 0);
@@ -20,17 +71,19 @@ public class Solution {
                 }
             }
         }
+
         return false;
     }
 
     public boolean check(char[][] board, boolean[][] visited, int i, int j, String s, int k) {
+
         if (board[i][j] != s.charAt(k)) {
             return false;
         } else if (k == s.length() - 1) {
             return true;
         }
         visited[i][j] = true;
-        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
         boolean result = false;
         for (int[] dir : directions) {
             int newi = i + dir[0], newj = j + dir[1];
@@ -47,5 +100,6 @@ public class Solution {
         visited[i][j] = false;
         return result;
     }
+
 
 }
