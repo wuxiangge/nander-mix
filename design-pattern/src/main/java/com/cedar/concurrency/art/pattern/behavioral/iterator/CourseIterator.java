761707bd466a05a226b12c2a63281e9ff9574986
zhangnan <1343887538@qq.com>
2021-08-02 13:42:07 +0800

    behavier

---
+++ design-pattern/src/main/java/com/cedar/concurrency/art/pattern/behavioral/iterator/CourseIterator.java
@@ -0,0 +1,10 @@
+package com.cedar.concurrency.art.pattern.behavioral.iterator;
+
+/**
+ * Created by geely.
+ */
+public interface CourseIterator {
+    Course nextCourse();
+    boolean isLastCourse();
+
+}
