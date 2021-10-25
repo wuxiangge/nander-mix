7219480353d939e9f25988fdc95e56a5df5555f3
zhangnx <zhangnx@servyou.com.cn>
2021-10-25 19:30:32 +0800

    字符拆分||

---
+++ leecode/src/main/java/labuladong/q1147/Solution.java
@@ -0,0 +1,60 @@
+package labuladong.q1147;
+
+/**
+ * @author zhangnan
+ * @date 2021/10/25
+ */
+public class Solution {
+
+    public int longestDecomposition(String text) {
+        return dfs(text, 0, text.length() - 1);
+    }
+
+    private int dfs(String text, int left, int right) {
+        if (left > right) {
+            return 0;
+        }
+
+        for (int i = left, j = right; i < j; i++, j--) {
+            if (text.substring(left, i + 1).equals(text.substring(j, right + 1))) {
+                return 2 + dfs(text, i + 1, j - 1);
+            }
+        }
+        return 1;
+    }
+
+
+    public int longestDecomposition2(String text) {
+        int n = text.length();
+        return dfs2(0, n - 1, text);
+    }
+
+
+    int dfs2(int begin, int end, String text) {
+        if (begin > end) {
+            return 0;
+        }
+
+        int j = end;
+        while (j > begin) {
+            int len = end - j + 1;
+
+            String left = text.substring(begin, begin + len);
+            String right = text.substring(j, len + j);
+
+            if (left.equalsIgnoreCase(right)) {
+                return 2 + dfs2(begin + len, j - 1, text);
+            }
+
+            j--;
+        }
+        return 1;
+    }
+
+
+    public static void main(String[] args) {
+        new Solution().longestDecomposition("abc");
+    }
+
+
+}
--- leecode/src/main/java/q140/Solution.java
+++ leecode/src/main/java/q140/Solution.java
@@ -9,9 +9,9 @@ import java.util.*;
 public class Solution {
 
     public List<String> wordBreak(String s, List<String> wordDict) {
-        Map<Integer, List<List<String>>> map = new HashMap<Integer, List<List<String>>>();
-        List<List<String>> wordBreaks = backtrack(s, s.length(), new HashSet<String>(wordDict), 0, map);
-        List<String> breakList = new LinkedList<String>();
+        Map<Integer, List<List<String>>> map = new HashMap<>();
+        List<List<String>> wordBreaks = backtrack(s, s.length(), new HashSet<>(wordDict), 0, map);
+        List<String> breakList = new LinkedList<>();
         for (List<String> wordBreak : wordBreaks) {
             breakList.add(String.join(" ", wordBreak));
         }
@@ -31,16 +31,17 @@ public class Solution {
      */
     public List<List<String>> backtrack(String s, int length, Set<String> wordSet, int index, Map<Integer, List<List<String>>> map) {
         if (!map.containsKey(index)) {
-            List<List<String>> wordBreaks = new LinkedList<List<String>>();
+            List<List<String>> wordBreaks = new LinkedList<>();
             if (index == length) {
-                wordBreaks.add(new LinkedList<String>());
+                // 判断终止
+                wordBreaks.add(new LinkedList<>());
             }
             for (int i = index + 1; i <= length; i++) {
                 String word = s.substring(index, i);
                 if (wordSet.contains(word)) {
                     List<List<String>> nextWordBreaks = backtrack(s, length, wordSet, i, map);
                     for (List<String> nextWordBreak : nextWordBreaks) {
-                        LinkedList<String> wordBreak = new LinkedList<String>(nextWordBreak);
+                        LinkedList<String> wordBreak = new LinkedList<>(nextWordBreak);
                         wordBreak.offerFirst(word);
                         wordBreaks.add(wordBreak);
                     }
