959ae06192238bae256c6561a9b6c0ec6476c4dd
zhangnan <1343887538@qq.com>
2021-10-26 08:46:28 +0800

    最长快乐前缀

---
+++ leecode/src/main/java/q187/Solution.java
@@ -0,0 +1,24 @@
+package q187;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Solution {
+
+    public List<String> findRepeatedDnaSequences(String s) {
+        HashSet<String> seen = new HashSet<>();
+        HashSet<String> res = new HashSet<>();
+
+        for (int i = 0; i + 10 <= s.length(); i++) {
+            String key = s.substring(i, i + 10);
+            if (seen.contains(key)) {
+                res.add(key);
+            } else {
+                seen.add(key);
+            }
+        }
+        return new ArrayList<>(res);
+    }
+
+}
